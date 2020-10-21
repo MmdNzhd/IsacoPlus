11f47d81e86b97de251b8c287df7dd94e0096c92
Mohammad Nezhadi <mmdnejadi@gmail.com>
2020-10-21 10:13:43 +0330

    Fucking Webscripping is finisheddddddddddd

--- Controllers/CallForPriceController.cs
+++ Controllers/CallForPriceController.cs
@@ -16,35 +16,55 @@ using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using WebScarpinInCSharp;
+using System.Threading;
 
 namespace KaraYadak.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public class CallForPriceController : Controller
     {
-        private readonly double itemsPerPage = 10;
-        private readonly ApplicationDbContext _context;
-        private readonly IWebHostEnvironment _env;
-
-
-        public CallForPriceController(ApplicationDbContext context, IWebHostEnvironment env)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public CallForPriceController(IWebHostEnvironment hostEnvironment)
         {
-            _context = context;
-            _env = env;
+            _hostEnvironment = hostEnvironment;
         }
 
-        [HttpPost]
-        //[Route("CallForPrice")]
-        [AllowAnonymous]
+        //[HttpPost]
+        //[AllowAnonymous]
         public  IActionResult CallForPrice(string productCode)
         {
             //webScraping
 
+            var url = _hostEnvironment.WebRootPath + "/Rar$EXa14112.24292";
+            //var webscrap = new webScrapp();
+            //var finalResult = webscrap.isacoWebscrap(productCode);
+            IWebDriver m_driver;
+            //m_driver = new ChromeDriver(@"C:\Users\MmdNzhd\AppData\Local\Temp\Rar$EXa0.659");
+            m_driver = new ChromeDriver(url);
 
-            var webscrap = new webScrapp();
-            var finalResult = webscrap.isacoWebscrap(productCode);
+            m_driver.Url = "https://www.isaco.ir/main/auto-parts-list/auto-parts-list/";
+            m_driver.Manage().Window.Maximize();
+            IWebElement button;
+            IWebElement finalTable;
+            Thread.Sleep(100);
 
-            return Json(finalResult);
+
+            m_driver.FindElement(By.Id("gdmCode")).SendKeys(productCode);
+            Thread.Sleep(100);
+
+            button = m_driver.FindElement(By.Id("post_form"));
+            Thread.Sleep(100);
+
+            button.Click();
+            Thread.Sleep(100);
+            finalTable = m_driver.FindElement(By.Id("result"));
+            Thread.Sleep(2000);
+
+            var innerHtml = finalTable.GetAttribute("innerHTML");
+            m_driver.Close();
+
+
+            return Json("<table class='table'>" + innerHtml + "</table>");
         }
         
 
---
+++ webScrapp.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebScarpinInCSharp
+{
+   
+    public class webScrapp 
+    {
+        public string isacoWebscrap(string productCode)
+        {
+            IWebDriver m_driver;
+            m_driver = new ChromeDriver(@"C:\Users\MmdNzhd\AppData\Local\Temp\Rar$EXa0.659");
+            //m_driver = new ChromeDriver(@"C:\Users\Administrator\AppData\Local\Temp\2\Rar$EXa8844.29816");
+
+            m_driver.Url = "https://www.isaco.ir/main/auto-parts-list/auto-parts-list/";
+            m_driver.Manage().Window.Maximize();
+            IWebElement button;
+            IWebElement finalTable;
+            Thread.Sleep(100);
+
+
+            m_driver.FindElement(By.Id("gdmCode")).SendKeys(productCode);
+            Thread.Sleep(100);
+
+            button = m_driver.FindElement(By.Id("post_form"));
+            Thread.Sleep(100);
+
+            button.Click();
+            Thread.Sleep(100);
+            finalTable = m_driver.FindElement(By.Id("result"));
+            Thread.Sleep(2000);
+
+            var innerHtml = finalTable.GetAttribute("innerHTML");
+            m_driver.Close();
+
+            return "<table class='table'>"+innerHtml+ "</table>";
+        }
+    }
+}
